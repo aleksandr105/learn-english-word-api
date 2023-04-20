7a3cc53b6493096c79a499209a54e5f7eeb6f2f1
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-04-20 10:48:31 +0200

    adds endpoint for request 10 words

---
+++ routes/api/wordsRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { getWords } = require("../../controllers");
+
+const router = express.Router();
+
+router.get("/", getWords);
+
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
