a56bff1ace4217df6100fc4d3ce187eb05d8a9e5
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-04-20 21:13:22 +0200

    fix getWord

--- controllers/wordControllers/getWords.js
+++ controllers/wordControllers/getWords.js
@@ -5,7 +5,7 @@ const getWords = async (_, res, next) => {
   try {
     const allWords = await Word.aggregate([{ $sample: { size: 10 } }]);
 
-    if (allWords) throw HttpError(404, "Not found");
+    if (!allWords) throw HttpError(404, "Not found");
 
     res.json(allWords);
   } catch (error) {
