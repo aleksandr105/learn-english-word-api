581fde47e223d7699487246010ac377b99624285
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-04-25 11:39:23 +0200

    ads cors settings

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://learn-english-words-api.onrender.com/api",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/words", wordsRouter);
