77d86527c7dc457bf4787657a3ceb3cd8dbcae4d
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-12-03 16:20:02 +0100

    fix pagination for get  words block list

--- controllers/wordControllers/getWordsBlockList.js
+++ controllers/wordControllers/getWordsBlockList.js
@@ -6,18 +6,17 @@ const getWordsBlockList = async (req, res) => {
     user: { _id },
   } = req;
 
-  const { page, limit } = req.query;
-
-  console.log(limit);
-
-  console.log(page);
+  const { page = 1, limit = 1 } = req.query;
 
   const skip = (page - 1) * limit;
 
-  const { blockList } = await UserWord.find({ owner: _id }, "blockList", {
-    skip,
-    limit: Number(limit),
-  });
+  const blockList = await UserWord.aggregate([
+    { $match: { owner: _id } },
+    { $unwind: "$blockList" },
+    { $project: { _id: 0, blockList: 1 } },
+    { $skip: skip },
+    { $limit: Number(limit) },
+  ]);
 
   if (!blockList) throw HttpError(404, "The document is not found");
 
