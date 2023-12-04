6bf2bbddead5162a2474589e902bdddaffb3e3dc
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-12-04 22:09:16 +0100

    adds pagination for search words in block list

--- controllers/wordControllers/searchWordsInBlockList.js
+++ controllers/wordControllers/searchWordsInBlockList.js
@@ -8,7 +8,39 @@ const searchWordsInBlockList = async (req, res) => {
     user: { _id },
   } = req;
 
-  const words = await UserWord.aggregate([
+  const { page = 1, limit = 3 } = req.query;
+
+  const skip = (page - 1) * limit;
+
+  // const words = await UserWord.aggregate([
+  //   { $match: { owner: _id } },
+  //   {
+  //     $project: {
+  //       matchingWords: {
+  //         $filter: {
+  //           input: "$blockList",
+  //           as: "word",
+  //           cond: {
+  //             $regexMatch: {
+  //               input: "$$word",
+  //               regex: searchText,
+  //               options: "i",
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  //   { $skip: skip },
+  //   { $limit: Number(limit) },
+  // ]);
+
+  const [
+    {
+      totalCount: [{ total }],
+      matchingWords: [{ matchingWords }],
+    },
+  ] = await UserWord.aggregate([
     { $match: { owner: _id } },
     {
       $project: {
@@ -20,18 +52,43 @@ const searchWordsInBlockList = async (req, res) => {
               $regexMatch: {
                 input: "$$word",
                 regex: searchText,
-                options: "i", // Для регистронезависимости
+                options: "i",
               },
             },
           },
         },
       },
     },
+    { $unwind: "$matchingWords" },
+    {
+      $facet: {
+        totalCount: [
+          {
+            $count: "total",
+          },
+        ],
+        matchingWords: [
+          { $skip: skip },
+          { $limit: parseInt(limit) },
+          {
+            $group: {
+              _id: null,
+              matchingWords: { $push: "$matchingWords" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              matchingWords: 1,
+            },
+          },
+        ],
+      },
+    },
   ]);
+  if (!matchingWords) throw HttpError(404, "The user or words is not found");
 
-  if (!words) throw HttpError(404, "The user or words is not found");
-
-  res.json(words[0].matchingWords);
+  res.json({ data: matchingWords, total });
 };
 
 module.exports = searchWordsInBlockList;
