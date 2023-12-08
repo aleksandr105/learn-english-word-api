a193c9e91317c9a73064cba5cec31f77e6c54d80
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-12-08 12:49:21 +0100

    adds paginate for controler getAllUserWords

--- controllers/wordControllers/getAllUserWords.js
+++ controllers/wordControllers/getAllUserWords.js
@@ -1,16 +1,24 @@
 const { UserWord } = require("../../models");
 const { HttpError } = require("../../helpers");
+const { object } = require("joi");
 
 const getAllUserWords = async (req, res) => {
   const {
     user: { _id },
   } = req;
 
+  const { page = 1, limit = 2 } = req.query;
+  const skip = (page - 1) * limit;
+
   const { userWords } = await UserWord.findOne({ owner: _id });
 
   if (!userWords) throw HttpError(404, "The document is not found");
 
-  res.json(userWords);
+  const data = userWords.map((el) => Object.keys(el)[1]);
+
+  const response = data.slice(Number(skip), Number(skip) + Number(limit));
+
+  res.json({ data: response, total: data.length });
 };
 
 module.exports = getAllUserWords;
